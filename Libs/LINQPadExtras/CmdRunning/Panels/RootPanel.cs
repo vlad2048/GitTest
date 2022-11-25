1db4965ab9cb19ce355372df2d487f6ce61e9c51
Vlad Niculescu <vlad.nic@gmail.com>
2022-11-25 21:26:04 +0000

    Initial commit

---
+++ Libs/LINQPadExtras/CmdRunning/Panels/RootPanel.cs
@@ -0,0 +1,34 @@
+﻿using CliWrap;
+using LINQPad;
+using LINQPad.Controls;
+using LINQPadExtras.Utils;
+
+namespace LINQPadExtras.CmdRunning.Panels;
+
+static class RootPanel
+{
+	private static DumpContainer dc = null!;
+
+	private static void OnRestart()
+	{
+		dc = new DumpContainer()
+			.StyleRootPanel()
+			.Dump();
+	}
+
+	public static CmdPanel MakeCmdPanel(string exeFile, string args, bool showCmdOnly)
+	{
+		RestartDetector.OnRestart(nameof(RootPanel), OnRestart);
+		var cmdPanel = new CmdPanel(exeFile, args, showCmdOnly);
+		dc.AppendContent(cmdPanel.Root);
+		return cmdPanel;
+	}
+
+	public static LogPanel MakeLogPanel()
+	{
+		RestartDetector.OnRestart(nameof(RootPanel), OnRestart);
+		var logPanel = new LogPanel();
+		dc.AppendContent(logPanel.Root);
+		return logPanel;
+	}
+}
